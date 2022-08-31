8ceb81316f2980aa7157af870857ba7da7c52199
anant721 <anantraj721@gmail.com>
2022-08-31 23:29:30 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.use(express.static("public/"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+const multer = require("multer");
+const path = require("path");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    let url = '';
+        if (file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg") {
+         url = "./public/image/";
+        }else if(file.mimetype == "video/x-flv" || file.mimetype == "video/mp4" || file.mimetype == "iapplication/x-mpegURL" || file.mimetype == "video/3gpp" || file.mimetype == "video/quicktime" || file.mimetype == "video/x-msvideo" || file.mimetype == "video/x-ms-wmv"){
+            url ="./public/video/";
+        }else{
+            url ="./public/others/"
+        }
+    cb(null, url);
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, uniqueSuffix + path.extname(file.originalname));
+  },
+});
+
+const upload = multer({ storage: storage });
+
+
+app.post("/upload", upload.single("file"), (req, res)=>{
+    res.json({
+        msg:"File successfully uploaded",
+        link:req.file.path
+    })
+});
+
+
+app.listen(PORT, () => {
+ console.log(`Server listening on port ${PORT}`);
+});
